e7f695159f789b95c22d60680bf1284ee913e051
stefani spayde <spayde.stefani@gmail.com>
2018-02-13 10:58:20 -0600

    moved nav to side, updated link style, added smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,12 +1,37 @@
-// <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-// <script src="https://github.com/kswedberg/jquery-smooth-scroll/blob/master/jquery.smooth-scroll.min.js"></script>
-// <script>
-// $('.smooth').on('click', function() {
-//     $.smoothScroll({
-//         scrollElement: $('body'),
-//         scrollTarget: '#' + this.id
-//     });
-//
-//     return false;
-// });
-// </script>
+var jump=function(e)
+{
+       //prevent the "normal" behaviour which would be a "hard" jump
+       e.preventDefault();
+       //Get the target
+       var target = $(this).attr("href");
+       //perform animated scrolling
+       $('html,body').animate(
+       {
+               //get top-position of target-element and set it as scroll target
+               scrollTop: $(target).offset().top
+       //scrolldelay: 2 seconds
+     },900,function()
+       {
+               //attach the hash (#jumptarget) to the pageurl
+               location.hash = target;
+       });
+
+}
+$(document).ready(function()
+{
+       $('a[href*=#]').bind("click", jump);
+       return false;
+
+});
+
+const navlinks = document.getElementById("navlinks");
+
+const sticky = navlinks.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navlinks.classList.add("sticky")
+  } else {
+    navlinks.classList.remove("sticky");
+  }
+}
